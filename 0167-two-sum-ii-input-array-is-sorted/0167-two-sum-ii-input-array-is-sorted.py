0fd147e72522fac8a46b9128c9260b2f07666662
EH Cho <49137648+dmsdms39@users.noreply.github.com>
2024-11-27 21:24:20 +0900

    Time: 1 ms (88.62%), Space: 18.2 MB (6.28%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,7 +1,8 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         partnerIdx = {}
-        for i, num in enumerate(numbers):
+        for i in range(len(numbers)):
+            num = numbers[i]
             if num in partnerIdx:
                 return [partnerIdx[num] + 1, i + 1]
             else:
