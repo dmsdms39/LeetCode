79ce289c15a9e97f76b266d635dd540430578c71
EH Cho <49137648+dmsdms39@users.noreply.github.com>
2024-12-01 23:48:52 +0900

    Time: 23 ms (5.05%), Space: 17.6 MB (5.11%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        if len(Counter(s)) != len(Counter(t)):
+            return False
+
+        def modifyStr(target):
+            dic = {}
+            result = ''
+
+            for i in range(len(target)):
+                result += dic.setdefault(target[i], str(i)+'.')
+
+            return result
+
+        s = modifyStr(s)
+        t = modifyStr(t)
+        print(s, t)
+
+        return True if s == t else False
